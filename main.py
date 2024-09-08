8825864133fe71fde3bff1f4cffb2d10e2c410ae
hxq7426 <67504461+hxq7426@users.noreply.github.com>
2024-09-08 20:00:31 +0800

    Update main.py
    remove words

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def get_words():
     return words.json()['data']['text']
 
 data = {
-    'msg': '生日推送@face=53@ \n------\n今日日期：{}\n今天是我来到世界的:{}天\n------\n'.format(date,get_count())+msg+'每日一言\n{}'.format(get_words())
+    'msg': '生日推送@face=53@ \n------\n今日日期：{}\n今天是我来到世界的:{}天\n------\n'.format(date,get_count())+msg
 }
 
 data1={
